7f760b4dd39b421090241c04dd5ee60732b7c923
GitPusher99 <robinyang.beijing@gmail.com>
2024-03-30 00:11:23 +0800

    fix: resolve runtime error

--- src/app/components/Swagger.tsx
+++ src/app/components/Swagger.tsx
@@ -1,13 +1,15 @@
 'use client';
-import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
+import dynamic from "next/dynamic";
 
 type Props = {
   spec: Record<string, any>,
 };
 
+const SwaggerUI = dynamic(() => import('swagger-ui-react'), { ssr: false });
+
 function Swagger({ spec }: Props) {
-  return <SwaggerUI spec={spec}  />;
+  return <SwaggerUI spec={spec}/>;
 }
 
 export default Swagger;
